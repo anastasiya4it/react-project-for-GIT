f41948a5ef5e5a74919e4d371db8b59eb1565010
Bo136 <anastasiya4it@gmail.com>
2023-10-15 00:15:49 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import './styles/App.css';
+import "./styles/App.css";
 import ReactDOM from "react-dom/client";
-import React, { useEffect, useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
 
-
-import Navbar from './components/UI/navbar/Navbar';
-import AppRouter from './components/AppRouter';
-import { AuthContext } from './context/context';
+import Navbar from "./components/UI/navbar/Navbar";
+import AppRouter from "./components/AppRouter";
+import { AuthContext } from "./context/context";
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
   const [isLogin, setIsLogin] = useState(true);
   useEffect(() => {
-    if (localStorage.getItem('auth')) {
+    if (localStorage.getItem("auth")) {
       setIsAuth(true);
       setIsLogin(false);
     }
-  }, [])
+  }, []);
   return (
-    <AuthContext.Provider value={{
-      isAuth,
-      setIsAuth: setIsAuth,
-      isLogin
-    }}>
-      <BrowserRouter >
+    <AuthContext.Provider
+      value={{
+        isAuth,
+        setIsAuth: setIsAuth,
+        isLogin,
+      }}
+    >
+      <BrowserRouter>
         <Navbar />
         <AppRouter />
-
       </BrowserRouter>
     </AuthContext.Provider>
-
   );
 }
 
--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,16 +3,13 @@ import { pablicRoutes } from '../router/pablicRoutes';
 import { privateRoutes } from '../router/privateRoutes';
 import { useRoutes } from 'react-router-dom';
 import { AuthContext } from "../context/context";
-import Loader from "./UI/loader/Loader";
+
 const AppRouter = () => {
-    const { isAuth, isLogin } = useContext(AuthContext);
-    // let isAuth = false;
+
+    const { isAuth, setIsAuth } = useContext(AuthContext);
     const privateAllRoutes = useRoutes(privateRoutes);
     const pablicAllRoutes = useRoutes(pablicRoutes);
-    if (isLogin) {
-        return <Loader />
-    }
-
+    
     return (
         (isAuth) ?
 
